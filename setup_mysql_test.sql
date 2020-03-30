31260b695367b4e302a495f59408aac3d784c327
Alouie412 <alouie412@gmail.com>
2020-03-30 07:26:22 +0000

    Fixed typo: Incorrect db name

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,7 +2,7 @@
 -- This is basically the exact same thing as setup_mysql_dev.sql
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_dev_db;
+USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
