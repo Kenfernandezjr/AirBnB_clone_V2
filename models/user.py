e3d2d55c9bbc6d654db734ba350a3067d1aacc43
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-01 23:31:36 +0000

    Upload test #4, edited user and removed Table

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Table, Column, String
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
