578fef3545d95c1eb9d5f3d3089283be32574057
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-02 04:32:13 +0000

    Upload test #3, city and user with relationship ref to place

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -18,3 +19,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='users')
