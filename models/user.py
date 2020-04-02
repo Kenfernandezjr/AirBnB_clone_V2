82c0836d61b42f1f6e2b95c4bbdc10e8aa7cc6ab
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-02 00:06:26 +0000

    Upload Test #5, removed default for the time being

--- models/user.py
+++ models/user.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchem2y import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class User(BaseModel, Base):
     """This is the class for user
     Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
+    email: email address
+    password: password for you login
+    first_name: first name
+    last_name: last name
     """
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
