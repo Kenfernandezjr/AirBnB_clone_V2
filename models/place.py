af49c0b87f8ccf53a868cdf55374c9c62b0144ff
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-03 03:03:36 +0000

    Upload test #1, to check if base.metabase is correct reversed.

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ place_amenity = Table('place_amenity', Base.metadata,
                              primary_key=True)
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
-                             primary_key=True)
+                             primary_key=True))
 
 
 class Place(BaseModel, Base):
