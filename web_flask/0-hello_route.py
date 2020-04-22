7bd518e848309019545c0752ad0d5116df9365ea
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-22 02:10:45 +0000

    Flask project number 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Script that starts the flask connection """
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -9,5 +9,6 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def hello_hbnb():
     return 'Hello HBNB!'
+
 
 app.run(host='0.0.0.0', port=5000)
