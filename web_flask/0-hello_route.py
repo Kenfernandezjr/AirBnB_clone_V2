8e9664746231d9075d25eb17b0101ed0bf738911
Kenfernandezjr <Kenfernandezjr@gmail.com>
2020-04-22 01:59:45 +0000

    init and hello_route python

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script that starts the flask connection """
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+app.run(host='0.0.0.0', port=5000)
